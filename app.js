aab3ff5cb875c08553df9c56f2a7f2d186c50385
Linno87 <linno824@gmail.com>
2023-07-10 22:15:06 -0300

    create: estructura

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const PORT= 3030;
+const app = express();
+
+
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => { console.log(`Server listening on http://localhost:${PORT}`)})
