805fa82750744598bf427556eb7077ea83eb64d1
Debora <escobardebo@gmail.com>
2023-07-14 21:57:47 -0300

    se crea rutas html

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ const app = express();
 
 app.use(express.static("public"));
 
-
+app.get("/detalle", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "detalle.html"))
+);
+app.get("/footer", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "footer.html"))
+);
 app.listen(PORT, () => { console.log(`Server listening on http://localhost:${PORT}`)})
