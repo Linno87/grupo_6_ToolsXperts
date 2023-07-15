0704933deed1425651e5b32f6ba5edcad6218f41
Debora <escobardebo@gmail.com>
2023-07-15 02:16:10 -0300

    Se crea hoja de css-, se comienza la estructura del footer

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+);
+
 app.get("/detalle", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "detalle.html"))
 );
