e128d21e09a77e7492e9e7af738f79daa48f1a3b
Debora <escobardebo@gmail.com>
2023-07-25 16:49:09 -0300

    se suben modificaciones tablet y desktop

--- public/js/detalle.js
+++ public/js/detalle.js
@@ -1,5 +1,12 @@
 
 var swiper = new Swiper(".mySwiper", {
+  breakpoints: {
+  
+    1024: {
+      slidesPerView: 0.5,
+      spaceBetween: 0,
+    },
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -9,9 +16,24 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 var swiper = new Swiper(".mySwiper2", {
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
   
+ 
