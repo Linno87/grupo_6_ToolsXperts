25e3a0f59d15fa5b3198f67cf6b4d61fc5ace387
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-08-04 21:24:59 -0300

    feat: refactorizar sprint con express generator

---
+++ src/controllers/productsController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    carrito : (req,res) => {
+        return res.render('carrito')
+    },
+    detalle : (req,res) => {
+        return res.render('detalle')
+    }
+}
---
+++ src/routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+const { register, login } = require('../controllers/usersControler');
+var router = express.Router();
+
+/* /users */
+router.get('/register', register);
+router.get('/login', login);
+
+module.exports = router;
