b00d4ee1a81d5cf2302bc6a70957c5efb80968cf
Linno87 <linno824@gmail.com>
2023-08-20 19:54:14 -0300

    create:vista producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,14 +1,24 @@
+const { readJson } = require("../data");
+
+readJson
+
 module.exports = {
-    carrito : (req,res) => {
-        return res.render('carrito')
-    },
-    detalle : (req,res) => {
-        return res.render('detalle')
-    },
-    createProduct : (req,res) => {
-        return res.render('createProduct')
-    },
-    editProduct : (req,res) => {
-        return res.render('editProduct')
-    }
-}
+  products: (req, res) => {
+    const listProduct = readJson("products.json");
+    return res.render("products", {
+      listProduct,
+    });
+  },
+  carrito: (req, res) => {
+    return res.render("carrito");
+  },
+  detalle: (req, res) => {
+    return res.render("detalle");
+  },
+  createProduct: (req, res) => {
+    return res.render("createProduct");
+  },
+  editProduct: (req, res) => {
+    return res.render("editProduct");
+  },
+};
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { carrito, detalle, createProduct, editProduct } = require('../controllers/productsController');
+const { carrito, detalle, createProduct, products, editProduct } = require('../controllers/productsController');
 const router = express.Router();
 
 /* /products */
+router.get('/', products)
 router.get('/carrito', carrito)
 router.get('/detalle', detalle)
 router.get('/createProduct', createProduct)
