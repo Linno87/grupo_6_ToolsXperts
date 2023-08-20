5708265b608f3c529feb622fd8fd0ec5d274c4e4
Linno87 <linno824@gmail.com>
2023-08-20 02:59:13 -0300

    create:product json

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+
+const {writeFileSync, readFileSync} = require('fs');
+const path = require('path');
+
+module.exports ={
+    readJson : (json) =>{
+        return JSON.parse(readFileSync(path.join(__dirname, json)))
+    },
+    writeJson : (list, name) =>{
+        writeFileSync(path.join(__dirname,name),JSON.stringify(list, null, 2),"utf-8");
+    }
+}
