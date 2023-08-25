c4db16c843ac5f6ea2df41d20ac7973977317534
Linno87 <linno824@gmail.com>
2023-08-25 18:43:50 -0300

    fix: require product

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const path = require("path");
-const product = require('Product')
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
