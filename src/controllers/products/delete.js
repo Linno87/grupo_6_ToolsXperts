e2d5d1021b4397a5705613a67818bb641c9ccd5b
MateoMariotti <mateomariotti77@gmail.com>
2023-08-25 16:36:30 -0600

    pruebaDelete

--- src/controllers/products/delete.js
+++ src/controllers/products/delete.js
@@ -2,7 +2,7 @@ const { readJson, writeJson} = require("../../data")
 
 module.exports = (req,res) => {
     const products = readJson('products.json');
-    const id = +req.params.id;
+    const id = req.params.id;
 
     const productsModify = products.filter(product => product.id !== id);
 
