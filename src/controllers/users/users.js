36d29e7cc966fb4fc267636085b680b0a041f17f
LeonardoGarcia-Estralagio <estralagio@gmail.com>
2023-09-01 17:39:06 -0300

    feat: entidad de usuarios

---
+++ src/controllers/users/users.js
@@ -0,0 +1,11 @@
+const { readJson } = require("../../data");
+
+module.exports = (req, res) => {
+  const listUser = readJson("users.json");
+  const id = req.params.id;
+  const users = listUser.find((user) => user.id === +id);
+  return res.render("users", {
+    listUser,
+    users,
+  });
+};
--- src/controllers/usersControler.js
+++ src/controllers/usersControler.js
@@ -1,19 +1,5 @@
-const { readJson } = require("../data");
-
 module.exports = {
-  register: (req, res) => {
-    return res.render("register");
-  },
-  login: (req, res) => {
-    return res.render("login");
-  },
-  users: (req, res) => {
-    const listUser = readJson("users.json");
-    const id = req.params.id;
-    const users = listUser.find((user) => user.id === +id);
-    return res.render("users", {
-      listUser,
-      users,
-    });
-  },
+  register: require('./users/register') ,
+  login: require('./users/login'),
+  users: require('./users/users'),
 };
