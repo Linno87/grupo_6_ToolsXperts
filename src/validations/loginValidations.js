a157e87bc66c5a03a4214bed0451a0fba33bd8d7
Debora <escobardebo@gmail.com>
2023-09-10 23:39:17 -0300

    se comienza a trabajar sobre las validaciones

--- src/validations/loginValidations.js
+++ src/validations/loginValidations.js
@@ -1,1 +1,9 @@
-const {check}= require('express-validator')
+const {body}= require('express-validator');
+const users = require('../data/users.json');
+
+module.exports=[
+    body("email").notEmpty().withMessage("El email es obligatorio").bail().isEmail().withMessage("Email no válido").bail(),
+    body("password").notEmpty().withMessage('La contraseña es requerida').bail()
+
+    
+]
