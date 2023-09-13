fe948cfaff330d1dc0cbe3aff90386a554036c3a
Rodrigo E. Hidalgo <rodrihidalgo20@gmail.com>
2023-09-12 22:03:27 -0300

    merge de validacion de register

--- src/data/User.js
+++ src/data/User.js
@@ -1,21 +1,23 @@
 const { v4: uuidv4 } = require("uuid");
 const { hashSync } = require("bcryptjs");
 
-const User = ({
-  firstName,
-  lastName,
-  email,
-  password,
-  categoryUser,
-  profile_image,
-}) => {
-  this.id = uuidv4();
-  this.firstName = firstName.trim();
-  this.lastName = lastName.trim();
-  this.email = email.trim();
-  this.password = password.hashSync(password, 12);
-  this.categoryUser = categoryUser;
-  this.profile_image = profile_image;
-};
+class User {
+  constructor({
+    firstName,
+    lastName,
+    email,
+    password,
+    categoryUser,
+    profile_image,
+  }) {
+    this.id = uuidv4();
+    this.firstName = firstName.trim();
+    this.lastName = lastName.trim();
+    this.email = email.trim();
+    this.password = hashSync(password, 8); 
+    this.categoryUser = categoryUser;
+    this.profile_image = profile_image;
+  }
+}
 
 module.exports = User;
--- src/validations/userValidator.js
+++ src/validations/userValidator.js
@@ -1,22 +1,27 @@
 const { check, body } = require("express-validator");
 
 module.exports = [
-  check("firstName").notEmpty().withMessage("El nombre es obligatorio"),
-  check("lastName").notEmpty().withMessage("El apellido es requerido"),
+  check("firstName").notEmpty().withMessage("El nombre es obligatorio").bail(),
+  check("lastName").notEmpty().withMessage("El apellido es requerido").bail(),
   check("date")
     .notEmpty()
+    .isNumeric()
     .withMessage("La fecha de nacimiento es obligatoria")
-    .isDate()
-    .withMessage("La fecha de nacimiento no es válida"),
+    .bail(),
   check("email")
     .notEmpty()
     .withMessage("El correo electrónico es obligatorio")
+    .bail()
     .isEmail()
     .withMessage("El correo electrónico no es válido"),
-  check("password").notEmpty().withMessage("La contraseña es obligatoria"),
+  check("password")
+    .notEmpty()
+    .withMessage("La contraseña es obligatoria")
+    .bail(),
   check("password_confirmation")
     .notEmpty()
     .withMessage("La confirmación de la contraseña es obligatoria")
+    .bail()
     .custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error(
@@ -27,10 +32,11 @@ module.exports = [
     }),
   check("categoryUser")
     .notEmpty()
-    .withMessage("Debes seleccionar una categoría"),
+    .withMessage("Debes seleccionar una categoría")
+    .bail(),
   body("profile_image").custom((value, { req }) => {
     if (!req.file) {
-      throw new Error("No has subido ninguna imagen");
+      throw new Error("No has subido ninguna imagen").bail();
     }
     return true;
   }),
