c9a822a159d5098f81de2b344fa6fcadd43d8db4
Rodrigo E. Hidalgo <rodrihidalgo20@gmail.com>
2023-09-13 15:46:54 -0300

    modificacion de nombre registerValidator

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { register, login, users, saveUser, processLogin } = require('../controllers/usersController');
 const uploadUser = require("../middlewares/uploadUser");
-const userValidator = require('../validations/userValidator')
+const userValidator = require('../validations/registerValidator')
 const router = express.Router();
 
 /* /users */
 router.get('/register', register);
-router.post("/register", uploadUser.single("profile_image"), userValidator, saveUser);
+router.post("/register", uploadUser.single("profile_image"), registerValidator, saveUser);
 router.get('/login', login);
 router.post('/login', processLogin);
 router.get("/", users);
