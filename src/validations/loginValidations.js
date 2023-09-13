cde928f0539a7aeeb0f37a2badbd51eef390c056
Debora <escobardebo@gmail.com>
2023-09-13 14:20:42 -0300

    Se crea archivo de validaciones backend

--- src/validations/loginValidations.js
+++ src/validations/loginValidations.js
@@ -1,9 +1,21 @@
 const {body}= require('express-validator');
 const users = require('../data/users.json');
+const { readJson, writeJson } = require("../../data");
+const { compareSync } = require('bcryptjs');
+
 
 module.exports=[
     body("email").notEmpty().withMessage("El email es obligatorio").bail().isEmail().withMessage("Email no válido").bail(),
-    body("password").notEmpty().withMessage('La contraseña es requerida').bail()
+    body("password").notEmpty().withMessage('La contraseña es requerida').bail().custom((value,{req})=>{
+        const usersFromJson = readJson('users.json');
+        const user =  usersFromJson.find(user=> user.email === req.body.email)/*usuario leido/usuario encontrado con find, compara el email ingresado con el email registrado en el json de usuarios */
+        if(!user || !compareSync(value, user.password)){
+            /* si el usuario es distinto y el password es distinto = usuario falso */
+            return false
+        }
+        return true/* si coincide las credenciales= se loguea */
+    }).withMessage('credenciales inválidas')
+
 
     
 ]
