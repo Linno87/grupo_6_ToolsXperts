c9126d0cd44ae3aecc7e87003439905957de82cd
Linno87 <linno824@gmail.com>
2023-12-14 13:27:39 -0300

    fix: agrego productos

--- src/database/seeders/20231017000417-products.js
+++ src/database/seeders/20231017000417-products.js
@@ -1,62 +1,26 @@
 "use strict";
 
+const productsJSON = require("../../data/products.json");
+const products = productsJSON.map(({name, description, price, discount,brandId,image, categoryId, rubroId}) => {
+  return {
+    name,
+    description,
+    price,
+    discount,
+    brandId,
+    Image: image,
+    categoryId,
+    rubroId,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+});
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "Products",
-      [
-        {
-          name: "Amoladora de Prueba",
-          description: "Amoladora de Prueba",
-          price: 600,
-          discount: 12,
-          brandId: 1,
-          Image: "1692921179529_products_.png",
-          categoryId: 1,
-          rubroId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Agujereadora de Prueba",
-          description: "Agujereadora de Prueba",
-          price: 1500,
-          discount: 0,
-          brandId: 1,
-          Image: "1692922324328_products_.jpeg",
-          categoryId: 1,
-          rubroId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Amoladora de Prueba",
-          description: "Amoladora de Prueba",
-          price: 800,
-          discount: 10,
-          brandId: 1,
-          Image: "1696078105141_products_.png",
-          categoryId: 1,
-          rubroId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          name: "Amoladora de Prueba",
-          description: "Amoladora de Prueba",
-          price: 2500,
-          discount: 0,
-          brandId: 1,
-          Image: "1698119947008_products_.jpg",
-          categoryId: 1,
-          rubroId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        }
-      ],
-      {}
-    );
+    await queryInterface.bulkInsert("Products",products,{});
   },
 
   async down(queryInterface, Sequelize) {
